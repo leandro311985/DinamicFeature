c2e74950814e5365c0d47722dc60b8dc6184c262
leandro.oliveira@cbyk.com.br <leandro.oliveira@cbyk.com.br>
2023-05-15 14:40:03 -0300

    commit firebase

---
+++ commons/baseApp/src/main/java/com/example/dinamicfeature/baseApp/constants/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.dinamicfeature.baseApp.constants
+
+
+object Constants {
+
+  private const val poolID = "Place_Your_UserPoolID"
+  private const val clientID = "Place_Your_ClientID"
+  private const val clientSecret = "Place_Your_ClientSecret"
+
+}
